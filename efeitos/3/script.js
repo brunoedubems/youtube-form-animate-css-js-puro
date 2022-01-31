8f8d8a4f29a1701862bab41a9aae18328569a329
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-01-31 10:58:35 -0300

    3

---
+++ efeitos/3/script.js
@@ -0,0 +1,13 @@
+const btnLogin = document.querySelector('.btn-login');
+const form = document.querySelector("form");
+
+btnLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+    form.classList.add("form-hide");
+});
+
+
+
+
+
+
